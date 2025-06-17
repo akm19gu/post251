913bc6f90ae0ec515fa3c8e5c389ce59e77c324b
akm19gu <Akm7339gil@gmail.com>
2025-06-17 16:02:27 +0900

    Update content-loader.js

--- js/content-loader.js
+++ js/content-loader.js
@@ -11,7 +11,7 @@ class ContentLoader {
 
     async loadContent() {
         try {
-            const base = "public/";
+            const base = "";
 
             const introContent   = await this.loadHtml(base + "サークル紹介.html");
             this.updateSection("introduction", introContent);
